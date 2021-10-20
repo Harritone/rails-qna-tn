dd89428e425afe8d8e8d8ff3cf450ed68f5d71f8
Hariton <yourrasik@gmail.com>
2021-10-20 15:52:12 +1000

    Add AnswersController#create and specs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,17 @@
 class AnswersController < ApplicationController
+  expose :answer
+  expose :question
+
+  def create
+    answer = question.answers.build(answer_params)
+    if answer.save
+      redirect_to answer.question
+    else
+      render :new
+    end
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :questions
+  resources :questions do
+    resources :answers, shallow: true, only: :create
+  end
+
 end
