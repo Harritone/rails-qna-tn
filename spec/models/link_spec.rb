6ecf178ad12c7227f8d31442c0385fdec01cec23
Hariton <yourrasik@gmail.com>
2021-10-31 17:43:35 +1000

    Add Link#gist

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
+  let(:link) { build(:link) }
+
   it { should belong_to(:linkable) }
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
 
   describe 'validation of url' do
-    let(:link) { build(:link) }
     let(:question) { create(:question) }
 
     before { link.linkable = question }
@@ -20,6 +21,18 @@ RSpec.describe Link, type: :model do
     it 'should not reurn error when url is valid' do
       link.url = 'http://valid.com'
       expect(link).to be_valid
+    end
+  end
+
+  describe '#gist?' do
+    it 'should return true if url include "gist.github.com"' do
+      link.url = 'https://gist.github.com/Harritone/170e91c1ce800c34417bc4ede3926f2a'
+      expect(link.gist?).to be true
+    end
+
+    it 'should return false if url not include "gist.github.com"' do
+      link.url = 'https://google.com'
+      expect(link.gist?).to be false
     end
   end
 end
