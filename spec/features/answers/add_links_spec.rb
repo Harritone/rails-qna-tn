9430f81b80676130d4093904aa42191561a9a46b
Hariton <yourrasik@gmail.com>
2021-11-01 00:29:27 +1000

    Add show gist when link refers to gist

--- spec/features/answers/add_links_spec.rb
+++ spec/features/answers/add_links_spec.rb
@@ -7,20 +7,40 @@ feature 'User can add links to answer', %q{
 } do
   given(:user) { create(:user) }
   given(:question) { create(:question, user: user) }
+  given(:google_url) { 'https://google.com' }
   given(:gist_url) { 'https://gist.github.com/Harritone/170e91c1ce800c34417bc4ede3926f2a' }
 
-  scenario 'User adds link when asks question', js: true do
+  background do
     login(user)
     visit question_path(question)
-
     fill_in 'Your answer', with: 'My awesome answer!'
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'URL', with: gist_url
+  end
+
+  scenario 'User adds link when publish answer', js: true do
+    fill_in 'Link name', with: 'My link'
+    fill_in 'URL', with: google_url
 
     click_on 'Publish'
 
     within '.answers' do
-      expect(page).to have_link 'My gist', href: gist_url
+      expect(page).to have_link 'My link', href: google_url
     end
+  end
+
+  scenario 'User adds invalid link when publish answer', js: true do
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'URL', with: 'Http/gisthub'
+    click_on 'Publish'
+
+    expect(page).to have_content 'is not a valid HTTP URL'
+  end
+
+  scenario 'User adds link to gist when publish answer', js: true do
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'URL', with: gist_url
+    click_on 'Publish'
+    visit current_path
+
+    expect(page).to have_content 'hosted with ❤ by GitHub'
   end
 end
--- spec/features/questions/add_links_spec.rb
+++ spec/features/questions/add_links_spec.rb
@@ -7,19 +7,39 @@ feature 'User can add links to question', %q{
 } do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/Harritone/170e91c1ce800c34417bc4ede3926f2a' }
+  given(:google_url) { 'https://google.com' }
 
-  scenario 'User adds link when asks question' do
+  background do
     login(user)
     visit new_question_path
 
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
+  end
 
-    fill_in 'Link name', with: 'My gist'
-    fill_in 'URL', with: gist_url
+  scenario 'User adds regular link when asks question' do
+    fill_in 'Link name', with: 'My link'
+    fill_in 'URL', with: google_url
 
     click_on 'Ask'
 
-    expect(page).to have_link 'My gist', href: gist_url
+    expect(page).to have_link 'My link', href: google_url
+  end
+
+  scenario 'User adds invalid link when asks question', js: true do
+    fill_in 'Link name', with: 'My link'
+    fill_in 'URL', with: 'Http//gisthub'
+
+    click_on 'Ask'
+
+    expect(page).to have_content 'is not a valid HTTP URL'
+  end
+
+  scenario 'User adds link to gist when asks question', js: true do
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'URL', with: gist_url
+    click_on 'Ask'
+
+    expect(page).to have_content 'hosted with ❤ by GitHub'
   end
 end
