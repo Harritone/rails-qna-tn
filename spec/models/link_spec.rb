2157e92071194e4a62b9e859b422bed1e1dbcde0
Hariton <yourrasik@gmail.com>
2021-10-31 16:24:44 +1000

    Add url validation for link

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Link, type: :model do
   it { should belong_to(:linkable) }
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  describe 'validation of url' do
+    let(:link) { build(:link) }
+    let(:question) { create(:question) }
+
+    before { link.linkable = question }
+
+    it 'should return errors when url is invalid' do
+      link.url = 'http:invalid.com'
+      expect(link).not_to be_valid
+      expect(link.errors[:url]).to include("is not a valid HTTP URL")
+    end
+
+    it 'should not reurn error when url is valid' do
+      link.url = 'http://valid.com'
+      expect(link).to be_valid
+    end
+  end
 end
